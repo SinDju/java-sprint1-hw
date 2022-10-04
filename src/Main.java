5facca2fab32e13154e884380cef7fbd0001ae7e
Olga Maslova <olga.maslova56@yandex.ru>
2022-10-04 07:30:25 +0300

    Added method saveStepsDay, printStepsMonthToDay

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,27 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         StepTracker stepTracker = new StepTracker();
-
+        int stepsMonthData = 0;
 
         while (true) {
             // обработка разных случаев
             printMenu(); // печатаем меню ещё раз перед завершением предыдущего действия
             int userInput = scanner.nextInt(); // повторное считывание данных от пользователя
             if (userInput == 1) {
+                System.out.println("За какой месяц вы хотите внести количество пройденных шагов: 0-Январь, 1-Февраль, 2-Март, 3-Апрель, " +
+                        "4-Май, 5-Июнь, 6-Июль, 7-Август, 8-Сентябрь, 9-Октябрь, 10-Ноябрь, 11-Декабрь?");
+                int month = scanner.nextInt();
+                System.out.println("Введите день месяца: от 1 до 30");
+                int day = scanner.nextInt();
+                System.out.println("Введите количество пройденных шагов:");
+                int stepsDay = scanner.nextInt();
+                stepsMonthData = stepTracker.saveStepsDay(month, day, stepsDay);
 
             } else if (userInput == 2) {
+                System.out.println("За какой месяц вы хотите внести количество пройденных шагов: 0-Январь, 1-Февраль, 2-Март, 3-Апрель, " +
+                        "4-Май, 5-Июнь, 6-Июль, 7-Август, 8-Сентябрь, 9-Октябрь, 10-Ноябрь, 11-Декабрь?");
+                int month = scanner.nextInt();
+                stepTracker.printStepsMonthToDay(month);
 
             } else if (userInput == 3) {
                 System.out.println("Введите Вашу новую цель по количеству шагов за день:");
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 
 public class StepTracker {
 
-    MonthData[] monthToData;
+    int[][] monthToData;
     int targetSteps = 10000; // может надо перенести в майн
 
+    public StepTracker() {
+        monthToData = new int [12][30];
+        for (int i = 0; i < monthToData.length; i++) {
+            for (int j = 0; j < monthToData[0].length; j++) {
+                monthToData[i][j] = 0;
+            }
+        }
+    }
      int TargetSteps (int steps) {
+
         if (steps>0) {
             System.out.println("Ваша новая цель по количеству шагов на день: "+ steps);
         }
@@ -15,15 +24,46 @@ public class StepTracker {
         return steps;
         // метод меняет цель по шагам
     }
-
-     public StepTracker() {
-        monthToData = new MonthData[12];
-        for (int i = 0; i < monthToData.length; i++) {
-            monthToData[i] = new MonthData();
+    int saveStepsDay(int month, int day, int stepsDay) {
+        //Как реализовать- Если за какой-то день статистика
+        // не сохранялась, то считаем количество шагов в этот день равным 0.
+        if (stepsDay >0) {
+            monthToData[month][day-1] = stepsDay;
+            System.out.println("Месяц: " + month + "." + " День: " + day + "." + " Сделано "
+                    + monthToData[month][day-1] + " шагов.");
         }
+        else {
+            System.out.println("Извините, введеное количества шагов не может быть отрицательным.");
+        }
+        return monthToData[month][day-1] ;
+    }
+
+    void printStepsMonthToDay(int month) {
+
+            for (int j = 0; j < 30; j++) {
+                System.out.println("Месяц: "+ month + "." + "День: "+ (j+1)  + "." +  "Сделано "
+                        + monthToData[month][j]  + " шагов.");
+            }
+            System.out.println();//перенос строки ради визуального сохранения табличной формы
+
     }
 
     class MonthData {
-        // Заполните класс самостоятельно
+
+         void saveStepsDay() {
+             /*Сохранение количества шагов за день. Пользователь должен указать
+             номер месяца (начиная с 0), номер дня и количество шагов, пройденных
+             в этот день. Количество шагов должно быть неотрицательным. Для
+             ускорения прототипирования на данном этапе считается, что в месяце
+             ровно 30 дней. Если за какой-то день статистика не сохранялась,
+             то считаем количество шагов в этот день равным 0.
+              */
+
+
+         }
+
+
+         // создать метод для вывода шагов за опред месяц + метод для внесения количества шагов за опред месяц
+
     }
 }
